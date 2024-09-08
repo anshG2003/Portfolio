3914fae60d80386bd69ef4195ab87cb478a94246
Ansh Gupta <anshgupta8004@gmail.com>
2024-09-08 15:42:28 +0530

    Updated Files

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,8 +1,29 @@
-import React from "react"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import { Heading } from "../common/Heading"
 import { contact } from "../data/dummydata"
 
 export const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_pef4513', 'template_rb2d8vf', form.current, {
+        publicKey: 'J7-pNUBTfOYxKPk39',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <>
       <div className='contact'>
@@ -10,13 +31,13 @@ export const Contact = () => {
           <Heading title='Keep In Touch' />
           <div className='content flexsb'>
             <div className='right'>
-              <form>
+              <form ref={form} onSubmit={sendEmail}>
                 <div className='flex'>
-                  <input type='text' placeholder='Name' data-aos='flip-left' />
-                  <input type='email' placeholder='Email' data-aos='flip-right' />
+                  <input type='text' name="to_name" placeholder='Name' data-aos='flip-left' />
+                  <input type='email' name="from_name" placeholder='Email' data-aos='flip-right' />
                 </div>
-                <input type='email' placeholder='Subject' data-aos='flip-up' />
-                <textarea name='' id='' cols='30' rows='10' data-aos='flip-down'></textarea>
+                <input type='message' placeholder='Subject' data-aos='flip-up' />
+                <textarea name='message' id='' cols='30' rows='10' data-aos='flip-down'></textarea>
                 <button data-aos='zoom-in-up'>Submit</button>
               </form>
             </div>
@@ -34,4 +55,4 @@ export const Contact = () => {
       </div>
     </>
   )
-}
+};
